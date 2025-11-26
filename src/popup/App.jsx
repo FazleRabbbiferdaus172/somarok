501709898f6b1ca12bee8b88664e461290fd9b5f
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-11-25 22:46:30 -0600

    bookmark call backs are added

--- src/popup/App.jsx
+++ src/popup/App.jsx
@@ -5,13 +5,19 @@ import IconButton from '@mui/material/IconButton';
 import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
 import BookmarkRemoveIcon from '@mui/icons-material/BookmarkRemove';
 function App() {
+  function handleAddBookmark(ev) {
+    console.log('Add bookmark');
+  }
 
+  function handleRemoveBookmark(ev) {
+     console.log('Remove bookmark');    
+  }
   return (
     <Container fixed>
         <Box sx={{ bgcolor: '#cfe8fc', height: '10vh', weidth: '2vw' }}>
             <IconButton>
-              <BookmarkAddIcon/>
-              <BookmarkRemoveIcon/>
+              <BookmarkAddIcon onClick={(ev) => handleAddBookmark(ev)}/>
+              <BookmarkRemoveIcon onClick={(ev) => handleRemoveBookmark(ev)}/>
             </IconButton>
         </Box>
       </Container>
