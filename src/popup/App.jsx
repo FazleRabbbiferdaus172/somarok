6df00915e949e33785f6365694ea2074178942d8
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-11-30 18:59:04 -0600

    button update

--- src/popup/App.jsx
+++ src/popup/App.jsx
@@ -1,13 +1,36 @@
 import './App.css';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import IconButton from '@mui/material/IconButton';
 import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
 import BookmarkRemoveIcon from '@mui/icons-material/BookmarkRemove';
 function App() {
+  const [isBookMarkAddActive, setIsBookMarkAddActive] = useState(false);
+  const [isBookMarkRemoveActive, setIsBookMarkRemoveActive] = useState(false);
+
+  function toggleBookmarkAdd() {
+    setIsBookMarkAddActive(!isBookMarkAddActive);
+  }
+
+  function toggleBookmarkRemove() {
+    setIsBookMarkRemoveActive(!isBookMarkRemoveActive);
+  }
+
+  function toggleBothBookmarkActions(ev) {
+    if (ev.target.id === 'add') {
+      toggleBookmarkAdd();
+      setIsBookMarkRemoveActive(false);
+    }
+    else if (ev.target.id === 'remove') {
+      toggleBookmarkRemove();
+      setIsBookMarkAddActive(false);
+    }
+  }
+
   function handleAddBookmark(ev) {
     console.log(`Add bookmark @ ${ev.clientX}, ${ev.clientY}`);
-    chrome.storage.local.set({ bookmark1: {x: ev.clientX, y: ev.clientY } }).then(() => {
+    chrome.storage.local.set({ bookmark1: { x: ev.clientX, y: ev.clientY } }).then(() => {
       console.log("Value is set");
     });
   }
@@ -19,8 +42,12 @@ function App() {
     <Container fixed>
       <Box sx={{ bgcolor: '#cfe8fc', height: '10vh', weidth: '2vw' }}>
         <IconButton>
-          <BookmarkAddIcon onClick={(ev) => handleAddBookmark(ev)} />
-          <BookmarkRemoveIcon onClick={(ev) => handleRemoveBookmark(ev)} />
+          <div id="add" onClick={(ev) => toggleBothBookmarkActions(ev)}>
+            <BookmarkAddIcon />
+          </div>
+          <div  id="remove" onClick={(ev) => toggleBothBookmarkActions(ev)} >
+            <BookmarkRemoveIcon/>
+          </div>
         </IconButton>
       </Box>
     </Container>
