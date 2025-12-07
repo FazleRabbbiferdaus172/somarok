b09a6c58ddc5a25d5e52691ea6386147f15514e6
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-12-06 20:57:00 -0600

    bookmark location added on click of docuemnt

--- src/content/main.jsx
+++ src/content/main.jsx
@@ -2,3 +2,8 @@ chrome.storage.local.get(["bookmark1"]).then((result) => {
   console.log("Value is " + JSON.stringify(result.bookmark1));
 });
 
+document.addEventListener("click", function (ev) {
+  const values = {location: window.location.href, xposition: ev.clientX, yposiiton: ev.clientY};
+  chrome.storage.local.set({ bookmark1: values });
+});
+
