997b9e1f3222d23cfe2c344bd920dda4db126567
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-12-07 20:59:19 -0600

    flags are added

--- src/content/main.jsx
+++ src/content/main.jsx
@@ -2,8 +2,16 @@ chrome.storage.local.get(["bookmark1"]).then((result) => {
   console.log("Value is " + JSON.stringify(result.bookmark1));
 });
 
+let isBookMarkAddActive = false;
+let isBookMarkRemoveActive = false;
+
 document.addEventListener("click", function (ev) {
   const values = {location: window.location.href, xposition: ev.clientX, yposiiton: ev.clientY};
-  chrome.storage.local.set({ bookmark1: values });
+  if (isBookMarkAddActive) {
+    chrome.storage.local.set({ bookmark1: values });
+  }
+  else if (isBookMarkRemoveActive) {
+    chrome.storage.local.remove("bookmark1");
+  }
 });
 
