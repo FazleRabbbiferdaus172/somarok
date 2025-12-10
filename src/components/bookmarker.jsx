20ab8aa3c7d5bb992445deaed8ceff4277f312c5
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-12-09 22:23:38 -0600

    minimal bookmarker logo

---
+++ src/components/bookmarker.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import bookmarker from '@/assets/bookmark.svg'
+
+export default function Bookmarker(props) {
+
+  return (
+    <>
+        <img src={bookmarker} className="logo" alt="Vite logo" />
+    </>
+  )
+}
