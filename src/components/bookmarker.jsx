9523e5bb6be0e3266f564e9a6b9e56661c2853b6
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-12-11 22:25:08 -0600

    state for remove added

--- src/components/bookmarker.jsx
+++ src/components/bookmarker.jsx
@@ -3,6 +3,7 @@ import bookmarker from '@/assets/bookmark.svg'
 
 export default function Bookmarker(props) {
     const [color, setColor] = useState('red');
+    const [canremove, setCanremove] = useState(false);
   return (
     <>
         <img src={bookmarker} className="logo" alt="Vite logo" style={{'color': color}}/>
