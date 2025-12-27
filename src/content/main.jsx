95fa13d6d36e638b3a1d158b081196d29c86fde2
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-12-27 00:18:34 -0600

    cleanups

--- src/content/main.jsx
+++ src/content/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot, StrictMode } from 'react-dom/client'
 import App from '@/content/views/App.jsx'
 
-var allBookmarks = [];
 let operation = null;
 
 const messageListener = (message, sender, sendResponse) => {
@@ -24,32 +23,8 @@ const messageListener = (message, sender, sendResponse) => {
     operation = 'remove';
   }
 }
+
 chrome.runtime.onMessage.addListener(messageListener);
-
-chrome.storage.local.get(["bookmark1"]).then((result) => {
-  console.log("Value is " + JSON.stringify(result.bookmark1));
-});
-
-chrome.storage.local.get(["allBookmarks"]).then((result) => {
-  if (result === undefined) {
-    allBookmarks = [];
-  } else {
-    allBookmarks = result.allBookmarks;
-  }
-});
-
-let isBookMarkAddActive = false;
-let isBookMarkRemoveActive = false;
-
-// document.addEventListener("click", function (ev) {
-//   const values = { location: window.location.href, xposition: ev.clientX, yposiiton: ev.clientY };
-//   if (isBookMarkAddActive) {
-//     chrome.storage.local.set({ bookmark1: values });
-//   }
-//   else if (isBookMarkRemoveActive) {
-//     chrome.storage.local.remove("bookmark1");
-//   }
-// });
 
 const container = document.createElement('div')
 container.id = 'somarok-extension-container'
