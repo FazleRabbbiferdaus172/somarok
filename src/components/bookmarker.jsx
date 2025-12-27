c1eca93e916c8ab63653d22cfcab69951b5d8d46
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-12-26 23:55:49 -0600

    position traslation to center for image

--- src/components/bookmarker.jsx
+++ src/components/bookmarker.jsx
@@ -5,7 +5,7 @@ export default function Bookmarker(props) {
     const [color, setColor] = useState('red');
     const [canremove, setCanremove] = useState(false);
   return (
-    <div className="somarok-bookmark" style={{position: 'absolute', top: props.yposition + 'px', left: props.xposition + 'px', zIndex: 1000}}>
+    <div className="somarok-bookmark" style={{position: 'absolute', top: props.yposition + 'px', left: props.xposition + 'px', zIndex: 1000, transform: 'translate(-25%, -50%)'}}>
         <img src={bookmarker} className="logo" alt="Vite logo" style={{'color': color}} width="100" height="100"/>
         {canremove ?
         <button>Remove</button> : <></>
