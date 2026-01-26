756da2e295a3cc00a631fc513a37ae36614a780d
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2026-01-25 18:44:38 -0600

    refactor and clean data

--- src/content/views/App.jsx
+++ src/content/views/App.jsx
@@ -2,42 +2,35 @@ import { StrictMode, useEffect, useState } from 'react';
 import Bookmarker from '@/components/bookmarker.jsx';
 
 export default function App() {
-  const [bookmarkerState, setbookmarkerList] = useState([]);
+  const [somaroks, setSomaroks] = useState([]);
   const [somarokState, setSomarokState] = useState(0);
+  const [location, setLocation] = useState(window.location.href);
 
   async function handleRemoveBookmark(xposition, yposition, location) {
-    const storageKey_location = location || window.location.href;
-    const location_somarok = await chrome.storage.local.get([storageKey_location]);
-    const allBookmarks = location_somarok[storageKey_location].filter(bookmark => !(bookmark.xposition === xposition && bookmark.yposition === yposition));
-    await chrome.storage.local.set({ [storageKey_location]: allBookmarks });
+    const locationSomarok = await chrome.storage.local.get([location]);
+    const allBookmarks = locationSomarok[location].filter(bookmark => !(bookmark.xposition === xposition && bookmark.yposition === yposition));
+    await chrome.storage.local.set({ [location]: allBookmarks });
   }
 
-  async function getDataFromStorage() {
-    let allBookmarks = [];
-    let bookmarkerList = [];
-    const storageKey_location = window.location.href;
-    const location_somarok = await chrome.storage.local.get([storageKey_location]);
-    if (location_somarok[storageKey_location] === undefined) {
-      allBookmarks = [];
+  async function loadDataFromStorageByLocation() {
+    let currentLocationSomaroks = [];
+    const allLocationSomaroks = await chrome.storage.local.get([location]);
+    if (allLocationSomaroks[location] === undefined) {
+      currentLocationSomaroks = [];
     } else {
-      allBookmarks = location_somarok[storageKey_location];
+      currentLocationSomaroks = allLocationSomaroks[location];
     }
-    allBookmarks.forEach((bookmark) => {
-      bookmarkerList.push(<Bookmarker key={bookmark.xposition} xposition={bookmark.xposition} yposition={bookmark.yposition} location={storageKey_location} onRemove={handleRemoveBookmark} />);
-    }
-    );
-    return bookmarkerList;
+    return currentLocationSomaroks;
   }
 
-  useEffect(() => {
-    const bookmarkerList = getDataFromStorage(bookmarkerState);
-    setbookmarkerList(bookmarkerList);
-  }, [somarokState]);
+  async function loadAndSetCurrentLocationSomaroks() {
+    const currentLocationSomaroks = await loadDataFromStorageByLocation();
+    setSomaroks(currentLocationSomaroks);
+  }
 
   useEffect(() => {
     const handleStorageChange = (changes, area) => {
-      const storageKey_location = window.location.href;
-      if (area === 'local' && changes[storageKey_location]) {
+      if (area === 'local' && changes[location]) {
         setSomarokState(prev => prev + 1);
       }
     };
@@ -49,9 +42,13 @@ export default function App() {
     };
   }, []);
 
+  useEffect(() => {
+    loadAndSetCurrentLocationSomaroks();
+  }, [somarokState, location]);
+
   return (
     <div>
-      {bookmarkerState.length !== 0 ? bookmarkerState : <></>}
+      {somaroks.map(somarok => { return <Bookmarker key={somarok.xposition} xposition={somarok.xposition} yposition={somarok.yposition} location={location} onRemove={handleRemoveBookmark} /> })}
     </div>
   )
 }
